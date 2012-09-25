4d7b7557577254d4914b9995e576ff54722e64ac
huangpengxiao <huangpengxiao@gmail.com>
2012-09-25 12:52:38 +0800

    ok

--- postcards/controllers/FilterViewController.m
+++ postcards/controllers/FilterViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "FilterViewController.h"
+#import "GPUImageView.h"
+#import "GPUImagePicture.h"
+#import "GPUImageSepiaFilter.h"
 
 #define FILTER_HEIGHT 60
 
@@ -15,6 +18,7 @@
 @end
 
 @implementation FilterViewController
+@synthesize image = _image;
 
 - (void)loadView
 {
@@ -28,11 +32,14 @@
     float imageWidth = 320;
     float imageHeight = ([self image].size.height / [self image].size.width) * imageWidth;
     view.frame = CGRectMake(0, 0, imageWidth, imageHeight);
-    [scroll setContentSize:CGSizeMake(imageWidth, imageHeight)];
+    [scroll setContentSize:CGSizeMake(imageWidth, imageHeight > (frame.size.height - 44) ? imageHeight : frame.size.height - 44)];
     [scroll addSubview:view];
     
     UIScrollView *horizonal = [[UIScrollView alloc] initWithFrame:CGRectMake(0, scroll.frame.size.height, frame.size.width, FILTER_HEIGHT)];
-    [horizonal setBackgroundColor:[UIColor redColor]];
+    horizonal.backgroundColor = [UIColor grayColor];
+    
+    [self renderFilters:horizonal];
+    
     [self.view addSubview:horizonal];
 }
 
@@ -46,6 +53,20 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - private selectors
+- (void)renderFilters:(UIView *) parentView
+{
+    GPUImageView *primaryView = [[GPUImageView alloc] initWithFrame:CGRectMake(5, 10, 40, 40)];
+    GPUImagePicture *sourcePicture = [[GPUImagePicture alloc] initWithImage:[self image] smoothlyScaleOutput:YES];
+    GPUImageSepiaFilter *sepiaFilter = [[GPUImageSepiaFilter alloc] init];
+    [sepiaFilter forceProcessingAtSize:primaryView.sizeInPixels]; // This is now needed to make the filter run at the smaller output size
+    [sourcePicture addTarget:sepiaFilter];
+    [sepiaFilter addTarget:primaryView];
+    
+    [sourcePicture processImage];
+    [parentView addSubview:primaryView];
 }
 
 @end
